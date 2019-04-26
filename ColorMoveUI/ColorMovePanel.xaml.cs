3e2286b8ee11928c1927b53e8c57096b9671e108
冉瑞元 <ruiyuan@nolovr.com>
2019-04-26 11:42:40 +0800

    同步

--- ColorMoveUI/ColorMovePanel.xaml.cs
+++ ColorMoveUI/ColorMovePanel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,37 @@ namespace ColorMoveUI
     /// <summary>
     /// ColorMovePanel.xaml 的交互逻辑
     /// </summary>
+    /// 
+
     public partial class ColorMovePanel : UserControl
     {
+        const int NCol = 5;
+        const int NRow = 8;
+        int[] ListData1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        DataTable ColorTable = new DataTable();
         public ColorMovePanel()
         {
             InitializeComponent();
+            InitData();
+            ColorDataGrid.DataContext = ColorTable;
+        }
+
+        private void InitData()
+        {
+            //DataColumn Column1 = new DataColumn("红色", typeof(Int32));
+            ColorTable.Columns.Add("红色");
+            ColorTable.Columns.Add("橙色");
+            ColorTable.Columns.Add("黄色");
+            ColorTable.Columns.Add("绿色");
+            ColorTable.Columns.Add("蓝色");
+            ColorTable.Rows.Add(new int[NCol] { 1, 2, 3, 4, 5 });
+            ColorTable.Rows.Add(new int[NCol] { 1, 2, 3, 4, 5 });
+            ColorTable.Rows.Add(new int[NCol] { 1, 2, 3, 4, 5 });
+            ColorTable.Rows.Add(new int[NCol] { 1, 2, 3, 4, 5 });
+            ColorTable.Rows.Add(new int[NCol] { 1, 2, 3, 4, 5 });
+            ColorTable.Rows.Add(new int[NCol] { 0, 0, 0, 0, 0 });
+            ColorTable.Rows.Add(new int[NCol] { 0, 0, 0, 0, 0 });
+            ColorTable.Rows.Add(new int[NCol] { 0, 0, 0, 0, 0 });
         }
     }
 }
